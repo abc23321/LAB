7ee2c748ac561dbc546db247e4db34bc34ff91e0
abc23321 <abcx12332@gmail.com>
2025-04-20 00:15:49 +0700

    LAB

---
+++ LAB1.1/Program.cs
@@ -0,0 +1,91 @@
+﻿//using System.Text;
+
+//Console.OutputEncoding = Encoding.UTF8;
+//// Bài 1
+//Console.Write("Nhập tên: ");
+//string ten = Console.ReadLine();
+//Console.Write("Nhập tuổi: ");
+//int tuoi = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Xin chào {ten}, bạn {tuoi} tuổi!");
+//// Bài 2
+//Console.Write("Nhập chiều dài: ");
+//int dai = int.Parse(Console.ReadLine());
+//Console.Write("Nhập chiều rộng: ");
+//int rong = int.Parse(Console.ReadLine());
+//int dientich = dai * rong;
+//Console.WriteLine($"Diện tích: {dientich}");
+//// Bài 3
+//Console.Write("Nhập độ C: ");
+//int c = int.Parse(Console.ReadLine());
+//int f = (c * 9 / 5) + 32;
+//Console.WriteLine($"Độ F: {f}");
+//// Bài 4
+//Console.Write("Nhập số nguyên: ");
+//int so = int.Parse(Console.ReadLine());
+//if (so % 2 == 0)
+//    Console.WriteLine($"{so} là số chẵn.");
+//else
+//    Console.WriteLine($"{so} là số lẻ.");
+//// Bài 5
+//Console.Write("Nhập số thứ nhất: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("Nhập số thứ hai: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Tổng: {a + b}, Tích: {a * b}");
+//// Bài 6
+//Console.Write("Nhập một số: ");
+//int x = int.Parse(Console.ReadLine());
+//if (x > 0)
+//    Console.WriteLine("Số dương.");
+//else if (x < 0)
+//    Console.WriteLine("Số âm.");
+//else
+//    Console.WriteLine("Số không.");
+//// Bài 7
+//Console.Write("Nhập năm: ");
+//int nam = int.Parse(Console.ReadLine());
+//if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+//    Console.WriteLine($"{nam} là năm nhuận.");
+//else
+//    Console.WriteLine($"{nam} không phải là năm nhuận.");
+//// Bài 8
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"\nBảng cửu chương {i}:");
+//    for (int j = 1; j <= 10; j++)
+//    {
+//        Console.WriteLine($"{i} x {j} = {i * j}");
+//    }
+//}
+//// Bài 9
+//Console.Write("Nhập số nguyên dương: ");
+//int n = int.Parse(Console.ReadLine());
+//long giaiThua = 1;
+//for (int i = 1; i <= n; i++)
+//{
+//    giaiThua *= i;
+//}
+//Console.WriteLine($"{n}! = {giaiThua}");
+//// Bài 10
+//Console.Write("Nhập một số nguyên: ");
+//int n2 = int.Parse(Console.ReadLine());
+//if (n2 < 2)
+//{
+//    Console.WriteLine($"{n2} không phải là số nguyên tố.");
+//}
+//else
+//{
+//    bool laNguyenTo = true;
+//    for (int i = 2; i <= Math.Sqrt(n2); i++)
+//    {
+//        if (n2 % i == 0)
+//        {
+//            laNguyenTo = false;
+//            break;
+//        }
+//    }
+//    if (laNguyenTo)
+//        Console.WriteLine($"{n2} là số nguyên tố.");
+//    else
+//        Console.WriteLine($"{n2} không phải là số nguyên tố.");
+//}
---
+++ LAB1.2/Program.cs
@@ -0,0 +1,121 @@
+﻿//using System.Text;
+
+//Console.OutputEncoding = Encoding.UTF8;
+////bài 1
+//static int TongSoChan(int[] arr)
+//{
+//    int tong = 0;
+//    foreach (int so in arr)
+//    {
+//        if (so % 2 == 0)
+//            tong += so;
+//    }
+//    return tong;
+//}
+//Console.Write("Nhập số lượng phần tử của mảng: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] mang = new int[n];
+
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write($"Nhập phần tử thứ {i + 1}: ");
+//    mang[i] = int.Parse(Console.ReadLine());
+//}
+
+//int tongChan = TongSoChan(mang);
+//Console.WriteLine($"Tổng các số chẵn trong mảng là: {tongChan}");
+////bài 2
+//static bool LaSoNguyenTo(int n)
+//{
+//    if (n < 2) return false;
+//    for (int i = 2; i <= Math.Sqrt(n); i++)
+//        if (n % i == 0)
+//            return false;
+//    return true;
+//}
+//Console.Write("Nhập số phần tử của mảng: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] arr = new int[n];
+//for (int i = 0; i < n; i++)
+//{
+//     Console.Write($"Nhập phần tử thứ {i}: ");
+//     arr[i] = int.Parse(Console.ReadLine());
+//}
+// Console.WriteLine("Các số nguyên tố trong mảng:");
+//for (int i = 0; i < n; i++)
+//{
+//     if (LaSoNguyenTo(arr[i]))
+//     {
+//         Console.WriteLine($"Chỉ số: {i}, Giá trị: {arr[i]}");
+//     }
+//}
+////bài 3
+//Console.Write("Nhập số phần tử: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] arr = new int[n];
+
+//int duong = 0, am = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write($"Nhập phần tử thứ {i}: ");
+//    arr[i] = int.Parse(Console.ReadLine());
+
+//    if (arr[i] > 0) duong++;
+//    else if (arr[i] < 0) am++;
+//}
+//Console.WriteLine($"Số dương: {duong} \nSố âm: {am}");
+////bài 4
+//Console.Write("Nhập số phần tử: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] arr = new int[n];
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write($"Nhập phần tử thứ {i}: ");
+//    arr[i] = int.Parse(Console.ReadLine());
+//}
+//int max1 = int.MinValue, max2 = int.MinValue;
+//foreach (int x in arr)
+//{
+//    if (x > max1)
+//    {
+//        max2 = max1;
+//        max1 = x;
+//    }
+//    else if (x > max2 && x != max1)
+//    {
+//        max2 = x;
+//    }
+//}
+//if (max2 == int.MinValue)
+//    Console.WriteLine("Không tồn tại số lớn thứ hai.");
+//else
+//    Console.WriteLine($"Số lớn thứ hai là: {max2}");
+////bài 5
+//static void HoanVi(ref int a, ref int b)
+//{
+//    int temp = a;
+//    a = b;
+//    b = temp;
+//}
+//Console.Write("Nhập số a: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("Nhập số b: ");
+//int b = int.Parse(Console.ReadLine());
+//HoanVi(ref a, ref b);
+//Console.WriteLine($"Sau khi hoán vị: a = {a}, b = {b}");
+////bài 6
+//Console.Write("Nhập số phần tử: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] arr = new int[n];
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write($"Nhập phần tử thứ {i}: ");
+//    arr[i] = int.Parse(Console.ReadLine());
+//}
+//Array.Sort(arr);
+//Console.WriteLine("Mảng sau khi sắp xếp:");
+//foreach (var x in arr)
+//{
+//    Console.Write($"{x} ");
+//}
